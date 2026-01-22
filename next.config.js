7dcd1865e52189426560d77cc3cebb99bcec1aac
laurent <asianrider@gmail.com>
2026-01-22 16:14:10 +0100

    Revert output directory

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
   output: 'export',
-  distDir: 'map',
+  distDir: 'out',
   trailingSlash: true,
   images: {
     unoptimized: true
